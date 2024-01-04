9c083a0e33713499334e387a690447ec80e952cf
Harsh <116268106+harshap0202@users.noreply.github.com>
2024-01-04 20:05:59 +0530

    first commit

---
+++ CellCelestial/src/main/java/com/ppf/project/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ppf.project.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.ppf.project.model.User;
+import com.ppf.project.dao.UserDao;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userDao.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user with email: " + email);
+        }
+        return new MyUserDetails(user);
+    }
+}
